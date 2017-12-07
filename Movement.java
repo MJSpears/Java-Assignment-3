83affacadd619504ef83b4866ee59ee6d268e96f
Matthew Spears <mjspears24@gmail.com>
2017-12-07 13:13:49 +0000

    Added classes for movement + sensor
    Currently classes with empty functions + variables

---
+++ Movement.java
@@ -0,0 +1,34 @@
+import Shefrobot.*;
+
+public class Movement {
+	private float distance = 0.0;
+	private Robot robot;
+
+	public void StartForward() {
+
+	}
+
+	public void StartBackward() {
+
+	}
+
+	public void RotateLeft() {
+
+	}
+
+	public void RotateRight() {
+
+	}
+
+	public void StopMovement() {
+
+	}
+
+	public float getDistance() {
+		return distance;
+	}
+
+	public void initRobot(Robot r) {
+		robot = r;
+	}
+}
---
+++ Sensor.java
@@ -0,0 +1,17 @@
+import ShefRobot.*;
+
+public class Sensor {
+	private Robot robot;
+
+	public boolean isBlack() {
+
+	}
+
+	public boolean isBlue() {
+
+	}
+
+	public void initRobot(Robot r) {
+		robot = r;
+	}
+}
