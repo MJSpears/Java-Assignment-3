c3a029186836bd3176c3af734aafae61be0a68fc
Matthew Spears <mjspears24@gmail.com>
2018-01-30 14:23:05 +0000

    Final version now Assignment3, added dance and working return

--- Assignment3.java
+++ Assignment3.java
@@ -1,31 +1,382 @@
 import ShefRobot.*;
 
-public class Assignment3 {
-	public static void main(String[] args) {
-		Robot robot = new Robot();
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-		Motor leftMotor = robot.getLargeMotor(Motor.Port.B);
-		Motor rightMotor = robot.getLargeMotor(Motor.Port.C);
-		//Motor gripMotor = robot.getMediumMotor(Motor.Port.A);
-		Speaker speaker = robot.getSpeaker();
-		ColorSensor sensor = robot.getColorSensor(Sensor.Port.S1);
-		TouchSensor button = robot.getTouchSensor(Sensor.Port.S2);
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-		leftMotor.setSpeed(360);
-		rightMotor.setSpeed(360);
+public class Assignment3 extends JFrame implements Runnable, KeyListener, WindowListener, ActionListener {
+	private static final int MOVING_SPEED = 300;
+	private static final int TURNING_SPEED = 200;
+	private static final int SEARCHING_SPEED = 150;
+	private static final int RETURN_SPEED = 500;
+	private static final int TURNING_TIME = 1500;
+	private static final int GRID_SIZE = 16;
+	private static final int SEGMENT_SIZE = 4;
+	private static final int OUTSIDE_SQUARE = 3;
 
-		ColorSensor.Color currentColor = sensor.getColor();
+	private static int redCount = 0;
+    private static int blueCount = 0;
+    private static int greenCount = 0;
+    private static int yellowCount = 0;
+    private static int brownCount = 0;
+	private static int[] colorCount = {redCount, blueCount, greenCount,
+																					yellowCount, brownCount};
+	private static boolean colorMatch = false;
+	private static boolean hasBall = false;
 
-		do {
-			System.out.println("not black");
-			robot.sleep(1000);
-			currentColor = sensor.getColor();
-			sensor.setMode(ColorSensor.Mode.COLOR);
-		} while (currentColor != ColorSensor.Color.BLACK);
+	private static float leftRotation = 0;
+	private static float rightRotation = 0;
 
-		System.out.println("found black");
+	Robot myRobot = new Robot();
 
-		robot.close();
+
+	//private static ColorSensor.Color[] colorGrid;
+
+	//Defining the behaviour of the prgram
+	enum Command {STOP, LEFT, RIGHT, FORWARD, REVERSE, DANCE };
+	private static final int DELAY_MS = 50;
+	
+	// Make the window, text label and menu
+	private static final int FRAME_WIDTH = 400;
+	private static final int FRAME_HEIGHT = 200;
+	
+	private JLabel label = new JLabel("Stop",JLabel.CENTER);
+			
+	public Assignment3() {
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("Quit");
+		JMenuItem menuItem = new JMenuItem("Really Quit?");
+		menuItem.addActionListener(this);
+		menu.add(menuItem);
+		menuBar.add(menu);
+		this.setJMenuBar(menuBar);
+		this.add(label, BorderLayout.CENTER);
+		label.setFont(new Font("SansSerif", Font.PLAIN, 48));
+		this.setBounds(0,0,FRAME_WIDTH,FRAME_HEIGHT);
+		this.setTitle("Sheffield Robot Football Controller");
+		this.addKeyListener(this);
+		this.addWindowListener(this);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+	
+	// Start the program	
+	private Command command = Command.STOP;	 
+
+	// Select the command corresponding to the key pressed	
+	public void keyPressed(KeyEvent e) {
+		switch ( e.getKeyCode()) {
+			case java.awt.event.KeyEvent.VK_UP:
+				command = Command.FORWARD;
+				break;
+			case java.awt.event.KeyEvent.VK_DOWN:
+				command = Command.REVERSE;
+				break;
+			case java.awt.event.KeyEvent.VK_LEFT:
+				command = Command.LEFT;
+				break;
+			case java.awt.event.KeyEvent.VK_RIGHT:
+				command = Command.RIGHT;
+				break;
+			case java.awt.event.KeyEvent.VK_SPACE:
+				command = Command.DANCE;
+				break;
+			default:
+				command = Command.STOP;
+				break;
+		}
+	}
+    //and released
+	public void keyReleased(KeyEvent e) {
+		command = Command.STOP;
+	}
+	//ignore everything else
+	public void keyTyped(KeyEvent e) {}	
+	public void windowActivated(WindowEvent e) {}
+	public void windowClosed(WindowEvent e) {}	
+	public void windowDeactivated(WindowEvent e) {}
+	public void windowDeiconified(WindowEvent e) {}
+	public void windowIconified(WindowEvent e) {}
+	public void windowOpened(WindowEvent e) {}
+	
+	// handle the quit menu item	
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("Really Quit?")) {
+			System.out.println("Closing Bluetooth");
+			myRobot.close();
+			System.exit(0);
+		}
+	}
+	//and the window closing
+	public void windowClosing(WindowEvent e) {
+		System.out.println("Closing Bluetooth");
+		myRobot.close();
+	}
+
+	public void storeColor(Robot myRobot, ColorSensor myColor) {
+		//currentColor = ColorSensor.getColor();
+		if (myColor.getColor() == ColorSensor.Color.RED) {
+				redCount++;
+			}
+			else if (myColor.getColor() == ColorSensor.Color.BLUE) {
+				blueCount++;
+			}
+			else if (myColor.getColor() == ColorSensor.Color.GREEN) {
+				greenCount++;
+			}
+			else if (myColor.getColor() == ColorSensor.Color.YELLOW) {
+				yellowCount++;
+		}
+	}
+
+	public void followCircle(Robot myRobot, Motor leftMotor, Motor rightMotor, Motor grabMotor, ColorSensor myColor, UltrasonicSensor myDistance) {
+		leftMotor.setSpeed(MOVING_SPEED);
+		rightMotor.setSpeed(MOVING_SPEED);
+		while (myColor.getColor() != ColorSensor.Color.BLACK) {
+			while (myColor.getColor() != ColorSensor.Color.WHITE) {
+				leftMotor.forward();
+			}
+			leftMotor.stop();
+			while (myColor.getColor() == ColorSensor.Color.WHITE) {
+				rightMotor.forward();
+			}
+			rightMotor.stop();
+		}
+		leftMotor.stop();
+		rightMotor.stop();
+		followLine(myRobot, leftMotor, rightMotor, grabMotor, myColor, myDistance);
+	}
+
+	public void checkColor() {
+		for (int counter : colorCount){
+			if (counter == 2) {
+				colorMatch = true;
+			}
+		}
+	}
+	public void findGrid(Robot myRobot, Motor leftMotor, Motor rightMotor, ColorSensor myColor) {
+		leftMotor.setSpeed(MOVING_SPEED);
+		rightMotor.setSpeed(MOVING_SPEED);
+
+		while (myColor.getColor() != ColorSensor.Color.BLACK) {
+			leftMotor.forward();
+			rightMotor.forward();
+		}
+		leftMotor.stop();
+		rightMotor.stop();
+		leftMotor.forward();
+		rightMotor.forward();
+		myRobot.sleep(500);
+		leftMotor.stop();
+		rightMotor.stop();
+
+
+		while (myColor.getColor() == ColorSensor.Color.WHITE) {
+			leftMotor.forward();
+		}
+		leftMotor.stop();
+		while (myColor.getColor() == ColorSensor.Color.BLACK) {
+			leftMotor.forward();
+		}
+		leftMotor.stop();
+	}
+
+
+	public void followLine(Robot myRobot, Motor leftMotor, Motor rightMotor, Motor grabMotor, ColorSensor myColor, UltrasonicSensor myDistance) {
+		leftMotor.setSpeed(MOVING_SPEED);
+		rightMotor.setSpeed(MOVING_SPEED);
+		while (!colorMatch && !hasBall){
+			while (myColor.getColor() == ColorSensor.Color.BLACK ||
+								myColor.getColor() == ColorSensor.Color.WHITE) {
+				while (myColor.getColor() == ColorSensor.Color.BLACK) {
+					leftMotor.forward();
+				}
+				leftMotor.stop();
+				while (myColor.getColor() == ColorSensor.Color.WHITE) {
+					rightMotor.forward();
+				}
+				rightMotor.stop();
+			}
+			storeColor(myRobot, myColor);
+			checkColor();
+			if (myColor.getColor() != ColorSensor.Color.BLUE){
+				followCircle(myRobot, leftMotor, rightMotor, grabMotor, myColor, myDistance);
+			}
+			else{
+				findBall(myRobot, leftMotor, rightMotor, grabMotor, myColor, myDistance);
+			}
+		}
 
 	}
+
+	public void findBall(Robot myRobot, Motor leftMotor, Motor rightMotor, Motor grabMotor, ColorSensor myColor, UltrasonicSensor myDistance) {
+		leftMotor.setSpeed(SEARCHING_SPEED);
+		rightMotor.setSpeed(SEARCHING_SPEED);
+		leftMotor.stop();
+		rightMotor.stop();
+		leftMotor.resetTachoCount();
+		rightMotor.resetTachoCount();
+
+		while (!hasBall) {
+			leftMotor.forward();
+			rightMotor.forward();
+
+			if (myDistance.getDistance() < 0.053) {
+				grabMotor.rotate(90);
+				hasBall = true;
+
+			}
+
+			if (myColor.getColor() == ColorSensor.Color.WHITE || myColor.getColor() == ColorSensor.Color.BLACK) {
+				leftMotor.rotate(360, true);
+				rightMotor.rotate(-360);
+				leftMotor.forward();
+				rightMotor.forward();
+				myRobot.sleep(100);
+			}
+
+			myRobot.sleep(100);
+		}
+	}
+
+	public void run() {
+		Motor leftMotor = myRobot.getLargeMotor(Motor.Port.B);
+		Motor rightMotor = myRobot.getLargeMotor(Motor.Port.C);
+		Motor grabMotor = myRobot.getMediumMotor(Motor.Port.D);
+		ColorSensor myColor = myRobot.getColorSensor(Sensor.Port.S1);
+		Speaker speaker = myRobot.getSpeaker();
+		UltrasonicSensor myDistance = myRobot.getUltrasonicSensor(Sensor.Port.S2);
+
+
+		label.setText("Running Automatically...");
+		findGrid(myRobot, leftMotor, rightMotor, myColor);
+		followLine(myRobot, leftMotor, rightMotor, grabMotor, myColor, myDistance);
+
+		label.setText("Found Ball!");
+		boolean hasFinished = false;
+		
+		leftMotor.setSpeed(RETURN_SPEED);
+		rightMotor.setSpeed(RETURN_SPEED);
+		while (!hasFinished) {
+			switch (command) {
+				case STOP:
+					label.setText("Stop");
+					leftMotor.stop();
+					rightMotor.stop();
+					
+					break;					
+				case FORWARD:
+					label.setText("Forward");
+					leftMotor.forward();
+					rightMotor.forward();
+					
+					break;					
+				case REVERSE:
+					label.setText("Reverse");
+					leftMotor.backward();
+					rightMotor.backward();
+					
+					break;					
+				case LEFT:
+					label.setText("Left");
+					leftMotor.backward();
+					rightMotor.forward();
+
+					break;
+				case RIGHT:
+					label.setText("Right");
+					leftMotor.forward();
+					rightMotor.backward();
+
+					break;
+				case DANCE:
+					label.setText("Dance");
+					hasFinished = true;
+
+ 					break;
+			}
+			try {
+				Thread.sleep(DELAY_MS);
+			} catch (InterruptedException e) {};
+		}
+
+		grabMotor.rotate(-90);
+		leftMotor.rotate(-360);
+		rightMotor.rotate(360);
+
+		myRobot.sleep(500);
+
+		leftMotor.rotate(-6200, true);
+		rightMotor.rotate(6200, true);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+		
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+		
+		speaker.playTone(622, 800);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 200);
+		speaker.playTone(622, 200);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		label.setText("Done!");
+	}
+
+	public static void main(String[] args) {
+		
+		
+		Thread t = new Thread(new Assignment3());
+		t.start();
+ 	}
 }
--- FollowLineTest.java
+++ FollowLineTest.java
@@ -19,16 +19,17 @@ public class FollowLineTest extends JFrame implements Runnable, KeyListener, Win
 	private static final int MOVING_SPEED = 300;
 	private static final int TURNING_SPEED = 200;
 	private static final int SEARCHING_SPEED = 150;
+	private static final int RETURN_SPEED = 500;
 	private static final int TURNING_TIME = 1500;
 	private static final int GRID_SIZE = 16;
 	private static final int SEGMENT_SIZE = 4;
 	private static final int OUTSIDE_SQUARE = 3;
 
 	private static int redCount = 0;
-  private static int blueCount = 0;
-  private static int greenCount = 0;
-  private static int yellowCount = 0;
-  private static int brownCount = 0;
+    private static int blueCount = 0;
+    private static int greenCount = 0;
+    private static int yellowCount = 0;
+    private static int brownCount = 0;
 	private static int[] colorCount = {redCount, blueCount, greenCount,
 																					yellowCount, brownCount};
 	private static boolean colorMatch = false;
@@ -37,14 +38,7 @@ public class FollowLineTest extends JFrame implements Runnable, KeyListener, Win
 	private static float leftRotation = 0;
 	private static float rightRotation = 0;
 
-	private static Robot myRobot = new Robot();
-	private static Motor leftMotor = myRobot.getLargeMotor(Motor.Port.B);
-	private static Motor rightMotor = myRobot.getLargeMotor(Motor.Port.C);
-	private static Motor grabMotor = myRobot.getMediumMotor(Motor.Port.D);
-	private static ColorSensor myColor = myRobot.getColorSensor(Sensor.Port.S1);
-	private static Speaker mySpeaker = myRobot.getSpeaker();
-	private static UltrasonicSensor myDistance = myRobot.getUltrasonicSensor(Sensor.Port.S2);
-	//private static GyroSensor myAngle = myRobot.getGyroSensor(Sensor.Port.S3);
+	Robot myRobot = new Robot();
 
 
 	//private static ColorSensor.Color[] colorGrid;
@@ -131,7 +125,7 @@ public class FollowLineTest extends JFrame implements Runnable, KeyListener, Win
 		myRobot.close();
 	}
 
-	public static void storeColor() {
+	public void storeColor(Robot myRobot, ColorSensor myColor) {
 		//currentColor = ColorSensor.getColor();
 		if (myColor.getColor() == ColorSensor.Color.RED) {
 				redCount++;
@@ -147,7 +141,7 @@ public class FollowLineTest extends JFrame implements Runnable, KeyListener, Win
 		}
 	}
 
-	public static void followCircle() {
+	public void followCircle(Robot myRobot, Motor leftMotor, Motor rightMotor, Motor grabMotor, ColorSensor myColor, UltrasonicSensor myDistance) {
 		leftMotor.setSpeed(MOVING_SPEED);
 		rightMotor.setSpeed(MOVING_SPEED);
 		while (myColor.getColor() != ColorSensor.Color.BLACK) {
@@ -162,18 +156,17 @@ public class FollowLineTest extends JFrame implements Runnable, KeyListener, Win
 		}
 		leftMotor.stop();
 		rightMotor.stop();
-		followLine();
+		followLine(myRobot, leftMotor, rightMotor, grabMotor, myColor, myDistance);
 	}
 
-	public static void checkColor() {
+	public void checkColor() {
 		for (int counter : colorCount){
 			if (counter == 2) {
 				colorMatch = true;
 			}
 		}
-		System.out.println(colorMatch);
 	}
-	public static void findGrid() {
+	public void findGrid(Robot myRobot, Motor leftMotor, Motor rightMotor, ColorSensor myColor) {
 		leftMotor.setSpeed(MOVING_SPEED);
 		rightMotor.setSpeed(MOVING_SPEED);
 
@@ -201,37 +194,35 @@ public class FollowLineTest extends JFrame implements Runnable, KeyListener, Win
 	}
 
 
-	public static void followLine() {
+	public void followLine(Robot myRobot, Motor leftMotor, Motor rightMotor, Motor grabMotor, ColorSensor myColor, UltrasonicSensor myDistance) {
 		
 		leftMotor.setSpeed(MOVING_SPEED);
 		rightMotor.setSpeed(MOVING_SPEED);
-		while (!colorMatch){
+		while (!colorMatch && !hasBall){
 			while (myColor.getColor() == ColorSensor.Color.BLACK ||
 								myColor.getColor() == ColorSensor.Color.WHITE) {
 				while (myColor.getColor() == ColorSensor.Color.BLACK) {
 					leftMotor.forward();
 				}
-				System.out.println("1 " + myColor.getColor());
 				leftMotor.stop();
 				while (myColor.getColor() == ColorSensor.Color.WHITE) {
 					rightMotor.forward();
 				}
 				rightMotor.stop();
 			}
-			System.out.println(myColor.getColor());
-			storeColor();
+			storeColor(myRobot, myColor);
 			checkColor();
 			if (myColor.getColor() != ColorSensor.Color.BLUE){
-				followCircle();
+				followCircle(myRobot, leftMotor, rightMotor, grabMotor, myColor, myDistance);
 			}
 			else{
-				findBall();
+				findBall(myRobot, leftMotor, rightMotor, grabMotor, myColor, myDistance);
 			}
 		}
 
 	}
 
-	public static void findBall() {
+	public void findBall(Robot myRobot, Motor leftMotor, Motor rightMotor, Motor grabMotor, ColorSensor myColor, UltrasonicSensor myDistance) {
 
 		leftMotor.setSpeed(SEARCHING_SPEED);
 		rightMotor.setSpeed(SEARCHING_SPEED);
@@ -244,7 +235,6 @@ public class FollowLineTest extends JFrame implements Runnable, KeyListener, Win
 			leftMotor.forward();
 			rightMotor.forward();
 
-			System.out.println(myDistance.getDistance());
 			if (myDistance.getDistance() < 0.053) {
 				grabMotor.rotate(90);
 				hasBall = true;
@@ -252,12 +242,6 @@ public class FollowLineTest extends JFrame implements Runnable, KeyListener, Win
 			}
 
 			if (myColor.getColor() == ColorSensor.Color.WHITE || myColor.getColor() == ColorSensor.Color.BLACK) {
-				//leftMotor.backward();
-				//rightMotor.backward();
-				//myRobot.sleep(200);
-				//while (myColor.getColor() == ColorSensor.Color.BLUE) {
-				//	myRobot.sleep(20);
-				//}
 				leftMotor.rotate(360, true);
 				rightMotor.rotate(-360);
 				leftMotor.forward();
@@ -270,15 +254,32 @@ public class FollowLineTest extends JFrame implements Runnable, KeyListener, Win
 	}
 
 	public void run() {
+
+		Motor leftMotor = myRobot.getLargeMotor(Motor.Port.B);
+		Motor rightMotor = myRobot.getLargeMotor(Motor.Port.C);
+		Motor grabMotor = myRobot.getMediumMotor(Motor.Port.D);
+		ColorSensor myColor = myRobot.getColorSensor(Sensor.Port.S1);
+		Speaker speaker = myRobot.getSpeaker();
+		UltrasonicSensor myDistance = myRobot.getUltrasonicSensor(Sensor.Port.S2);
+
+
+		label.setText("Following Line...");
+		findGrid(myRobot, leftMotor, rightMotor, myColor);
+
+		label.setText("Finding Ball...");
+		followLine(myRobot, leftMotor, rightMotor, grabMotor, myColor, myDistance);
+
+		label.setText("Found Ball!");
+		boolean hasFinished = false;
 		
-		while (true) {
+		leftMotor.setSpeed(RETURN_SPEED);
+		rightMotor.setSpeed(RETURN_SPEED);
+		while (!hasFinished) {
 			switch (command) {
 				case STOP:
 					label.setText("Stop");
 					leftMotor.stop();
 					rightMotor.stop();
-					
-					// put your code for stopping here
 					
 					break;					
 				case FORWARD:
@@ -286,37 +287,28 @@ public class FollowLineTest extends JFrame implements Runnable, KeyListener, Win
 					leftMotor.forward();
 					rightMotor.forward();
 					
-					// put your code for going forwards here
-					
 					break;					
 				case REVERSE:
 					label.setText("Reverse");
 					leftMotor.backward();
 					rightMotor.backward();
 					
-					// put your code for going backwards here
-					
 					break;					
 				case LEFT:
 					label.setText("Left");
 					leftMotor.backward();
 					rightMotor.forward();
-					
-  					// put your code for turning left here
 
 					break;
 				case RIGHT:
 					label.setText("Right");
 					leftMotor.forward();
 					rightMotor.backward();
-					
-    				// put your code for turning right here
 
 					break;
 				case DANCE:
 					label.setText("Dance");
-					
-    				// put your code for kicking here
+					hasFinished = true;
 
  					break;
 			}
@@ -324,12 +316,72 @@ public class FollowLineTest extends JFrame implements Runnable, KeyListener, Win
 				Thread.sleep(DELAY_MS);
 			} catch (InterruptedException e) {};
 		}
+
+		grabMotor.rotate(-90);
+		leftMotor.rotate(-360);
+		rightMotor.rotate(360);
+
+		myRobot.sleep(500);
+
+		leftMotor.rotate(-6200, true);
+		rightMotor.rotate(6200, true);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+		
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		speaker.playTone(622, 400);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 400);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+		
+		speaker.playTone(622, 800);
+		speaker.playTone(622, 200);
+		speaker.playTone(659, 200);
+		speaker.playTone(622, 200);
+		speaker.playTone(493, 200);
+		speaker.playTone(415, 200);
+
+		label.setText("Done!");
 	}
 
 	public static void main(String[] args) {
+		
+		
 		Thread t = new Thread(new FollowLineTest());
-		findGrid();
-		followLine();
 		t.start();
  	}
 }
